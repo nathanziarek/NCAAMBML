20607feeb12a16a55109e09d79290557ef0279c8
Nathan Ziarek <nziarek@directs.com>
2018-05-16 13:13:59 -0500

    averaging stats

--- buildCSV.js
+++ buildCSV.js
@@ -11,12 +11,49 @@ gameData = new Datastore({
 db.find({
     gameId: '1981773'
 }).exec(function (err, docs) {
+    console.log(docs)
     docs.map(function (d) {
         gameData.find({
-            name: new RegExp("("+d.teams[0]+")|("+d.teams[1]+")"),
-            date:{ $lt: d.date }
+            name: new RegExp("(" + d.teams[0] + ")|(" + d.teams[1] + ")"),
+            date: {
+                $lt: d.date,
+                $gt: findSeasonStart(d.date)
+            }
         }).exec((err, docs) => {
-            console.log(docs, docs.length);
+            var obj = {}
+            docs.map((d) => {
+
+
+                if (!obj[d.name]) {
+                    obj[d.name] = {}
+                }
+
+                obj[d.name].wins = Math.max(obj[d.name].wins || 0, d.wins);
+                obj[d.name].losses = Math.max(obj[d.name].losses || 0, d.losses);
+
+                ['score', 'fgm', 'fga', '3pm', '3pa', 'fta', 'ftm', 'oreb', 'dreb', 'ast', 'blk', 'to', 'pf', 'poss'].map((s) => {
+                    if (!obj[d.name][s]) {
+                        obj[d.name][s] = []
+                    }
+                    obj[d.name][s].push(d[s]);
+                })
+
+            });
+
+            for (team in obj) {
+                for (stat in obj[team]) {
+                    if (Array.isArray(obj[team][stat])) {
+                        obj[team][stat] = obj[team][stat].reduce(function (sum, a) {
+                            return sum + a
+                        }, 0) / (obj[team][stat].length || 1)
+                    }
+                }
+            }
+            console.log(obj);
+
+            //season starts in OCT
+            //so, if this is 10/11/12, then 
+            // need to find a way to keep us in the season. dont want to go back in time to last season...
             /*{ gameId: '1982007',
     date: 2017-11-11T01:00:00.000Z,
     'location-type': 'home',
@@ -46,4 +83,13 @@ db.find({
 
 function outputCSVLine() {
 
+}
+
+function findSeasonStart(date) {
+    var thisMonth = date.getMonth() + 1;
+    var thisYear = date.getFullYear();
+    if (thisMonth < 10) {
+        thisYear--;
+    }
+    return new Date('October 1, ' + thisYear);
 }
